d6c72585d67c64e40f7cb35971a7dd76059d9939
c0def1re <125857421+c0def1re@users.noreply.github.com>
2023-02-20 20:46:09 +1300

    Add files via upload

---
+++ setupnode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Welcome to the Yerbas node Install script v0.1a created by Codef1re"
+echo " "
+echo "Please enter the desired values at the prompts to continue"
+echo " "
+read -p "Please enter a name for the user account that will run the node: " uname
+read -p "Please enter a password for this account: " pword
+read -p "Please enter the IP addess of this node: " nodeip
+read -p "Please enter the BLSKey of this node: " BLSK
+read -p "How many node are already running on this VPS? (enter 0 for none): " numnodes
+echo "Thanks for the info, you entered the following"
